9fbe5fc1bd58894c3d0380fbaf1c6f5ad4ff136d
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2021-10-21 14:10:47 +0200

    Body scroll disabled when mobile menu is active

--- header.js
+++ header.js
@@ -15,11 +15,20 @@ window.addEventListener('scroll',function(){
 
 function mobileMenuToggle() {
     mainHeader.classList.toggle("mobile-nav-active");
+    toggleBodyScroll();
 }
 
 function mobileMenuClose() {
     mainHeader.classList.remove("mobile-nav-active");
+    toggleBodyScroll();
 }
 
 mobileMenuIcon.addEventListener("click",mobileMenuToggle);
 navLinks.forEach(elem => elem.addEventListener("click",mobileMenuClose));
+
+function toggleBodyScroll() {
+    if(mainHeader.classList.contains("mobile-nav-active"))
+        document.querySelector("body").style.overflow = "hidden";
+    else
+        document.querySelector("body").style.overflow = "auto";
+}

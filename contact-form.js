7dd7eb6470353908814981c237ca8b1667e0b135
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2021-10-21 22:15:04 +0200

    Removed jQuery as it is no longer necessary and used javascript to prevent redirect after form submission

--- contact-form.js
+++ contact-form.js
@@ -1,8 +1,21 @@
-$("form").on("submit", function() {
-    $.ajax({
-        url: "https://umberto-fontanazza.github.io/eidon/",
-        method: "POST",
-        data: {message: "hello!"},
-        dataType: "json"
-    });
-});
+var form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+      console.log("Form submitted successfully");
+      window.location = "./confirmation.html";
+    form.reset()
+  }).catch(error => {
+      console.log("Oops! There was a problem submitting your form");
+  });
+}
+
+form.addEventListener("submit", handleSubmit);
